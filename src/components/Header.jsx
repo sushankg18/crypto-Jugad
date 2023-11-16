6d841efeed684c0b696daa3faffcd664f5c352e3
Sushank Gautam <sushankg4@gmail.com>
2023-11-16 20:01:39 +0530

    added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,76 +1,218 @@
-import React from "react";
-import { HStack, Image, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Button, Flex, HStack, Image, Text } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import Logo from "../assets/CryptoJugad.png";
+import Logo from "../assets/New Logo.png";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaHome, FaCoins, FaExchangeAlt } from "react-icons/fa";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from "@chakra-ui/react";
 
 const Header = () => {
   const hoverEffect = {
     color: "white",
     transition: ".2s",
-    borderTop: "1px solid white",
+    borderBottom: "2px solid white",
   };
+
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setIsDrawerOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setIsDrawerOpen(false);
+  };
+
   return (
-    <HStack
-      h={"16"}
-      w={"full"}
-      px={"10"}
-      py={"10"}
-      fontSize={"large"}
-      color={"white"}
-      justifyContent={"space-between"}
-      bgColor={"#000"}
-      textAlign={"start"}
-    >
-      <Link to={"/"}>
-        <Image src={Logo} w={"48"} borderRadius={"5px"} />
-      </Link>
+    <>
       <HStack
-        gap={["", "", "8", "20"]}
-        fontWeight={"bold"}
-        display={["none", "flex"]}
+        h={"16"}
+        w={"full"}
+        px={["4", "10"]}
+        py={"10"}
+        fontSize={"large"}
+        color={"white"}
+        justifyContent={"space-between"}
+        bgColor={"#000"}
+        textAlign={"start"}
       >
         <Link to={"/"}>
-          <Text
-            _hover={hoverEffect}
-            fontSize={["none", ".8rem", "1rem", "1.2rem"]}
-            color={"#fff"}
-            fontWeight={"semibold"}
-            letterSpacing={"1px"}
-            py={"2"}
-            px={"4"}
-          >
-            Home
-          </Text>
+          <div style={{ position: "relative", display: "inline-block" }}>
+            <Image
+              src={Logo}
+              w={["40", "48"]}
+              filter={"grayscale(1)"}
+              borderRadius={"5px"}
+            />
+            <Text
+              _hover={{ color: "white" }}
+              fontSize={["none", ".8rem", "1rem", "1.2rem"]}
+              color={"#fff"}
+              fontWeight={"semibold"}
+              letterSpacing={"1px"}
+              py={"2"}
+              px={"4"}
+              position="absolute"
+              top="0"
+              right="0"
+            >
+              <sup
+                style={{
+                  fontSize: "0.6rem",
+                  marginLeft: "10px",
+                  position: "absolute",
+                  marginTop: "8px",
+                  fontFamily: "Josefin Sans",
+                }}
+              >
+                IND
+              </sup>
+            </Text>
+          </div>
         </Link>
-        <Link to={"/coins"}>
-          <Text
-            _hover={hoverEffect}
-            fontSize={["none", ".8rem", "1rem", "1.2rem"]}
-            color={"fff"}
-            px={"4"}
-            py={"2"}
-            fontWeight={"semibold"}
-            letterSpacing={"1px"}
-          >
-            Coins
-          </Text>
-        </Link>
-        <Link to={"/exchanges"}>
-          <Text
-            _hover={hoverEffect}
-            fontWeight={"semibold"}
-            fontSize={["none", ".8rem", "1rem", "1.2rem"]}
-            color={"fff"}
-            py={"2"}
-            objectFit={"contain"}
-            px={"4"}
-            letterSpacing={"1px"}
-          >
-            Exchanges
-          </Text>
-        </Link>
+
+        <Button
+          color={"black"}
+          display={["block", "block","none"]}
+          onClick={handleDrawerOpen}
+        >
+          <GiHamburgerMenu />
+        </Button>
+        <HStack
+          gap={["", "", "8", "20"]}
+          fontWeight={"bold"}
+          display={["none", "none", "flex", "flex"]}
+        >
+          <Link to={"/"}>
+            <Flex alignItems={"center"}>
+              <FaHome />
+
+              <Text
+                _hover={hoverEffect}
+                fontSize={["none", ".8rem", "1rem", "1.2rem"]}
+                color={"#fff"}
+                fontWeight={"semibold"}
+                letterSpacing={"1px"}
+                py={"2"}
+                px={"4"}
+              >
+                Home
+              </Text>
+            </Flex>
+          </Link>
+
+          <Link to={"/coins"}>
+            <Flex alignItems={"center"}>
+              <FaCoins />
+              <Text
+                _hover={hoverEffect}
+                fontSize={["none", ".8rem", "1rem", "1.2rem"]}
+                color={"fff"}
+                px={"4"}
+                py={"2"}
+                fontWeight={"semibold"}
+                letterSpacing={"1px"}
+              >
+                Coins
+              </Text>
+            </Flex>
+          </Link>
+
+          <Link to={"/exchanges"}>
+            <Flex alignItems={'center'}>
+              <FaExchangeAlt />
+              <Text
+                _hover={hoverEffect}
+                fontWeight={"semibold"}
+                fontSize={["none", ".8rem", "1rem", "1.2rem"]}
+                color={"fff"}
+                py={"2"}
+                objectFit={"contain"}
+                px={"4"}
+                letterSpacing={"1px"}
+              >
+                Exchanges
+              </Text>
+            </Flex>
+          </Link>
+        </HStack>
       </HStack>
-    </HStack>
+
+      {/* Drawer for smaller screens */}
+      <Drawer
+        isOpen={isDrawerOpen}
+        onClose={handleDrawerClose}
+        placement="right"
+      >
+        <DrawerOverlay />
+        <DrawerContent
+          bg={"black"}
+          color={"white"}
+          height={"fit-content"}
+          borderRadius={"10px"}
+          marginTop={"6"}
+          mr={"4"}
+        >
+          <DrawerCloseButton />
+          <DrawerHeader alignSelf={"center"}>Menu</DrawerHeader>
+          <DrawerBody>
+            <Link to={"/"}>
+              <Flex alignItems={"center"} gap={"4"}>
+                <FaHome />
+                <Text
+                  fontSize="1rem"
+                  color="white"
+                  fontWeight="semibold"
+                  letterSpacing="1px"
+                  py="2"
+                >
+                  Home
+                </Text>
+              </Flex>
+            </Link>
+            <Link to={"/coins"}>
+              <Flex alignItems={"center"} gap={"4"}>
+                <FaCoins />
+                <Text
+                  fontSize="1rem"
+                  color="white"
+                  fontWeight="semibold"
+                  letterSpacing="1px"
+                  py="2"
+                >
+                  Coins
+                </Text>
+              </Flex>
+            </Link>
+            <Link to={"/exchanges"}>
+              <Flex alignItems={"center"} gap={"4"}>
+                <FaExchangeAlt />
+                <Text
+                  fontSize="1rem"
+                  color="white"
+                  fontWeight="semibold"
+                  letterSpacing="1px"
+                  py="2"
+                >
+                  Exchanges
+                </Text>
+              </Flex>
+            </Link>
+          </DrawerBody>
+          <DrawerFooter>
+            {/* Add any additional footer content here */}
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 };
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-
+import {
+  Box,
+  HStack,
+  Container,
+  Flex,
+  Heading,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+import banner from "../assets/Banner.webp";
 const Home = () => {
   return (
-    <div>
-      HOME
-    </div>
-  )
-}
+    <Box w={"full"} height={"88vh"}>
+      <HStack
+        height={"80%"}
+        flexDirection={['column-reverse','row']}
+      >
+        <Flex h={'full'} w={"full"} justifyContent={"center"} alignItems={"center"}>
+          <Heading fontFamily={'Josefin Sans'} fontSize={['large','xxx-large']}>BUY TRADE ON CRYPTOJUGAD</Heading>
+        </Flex>
+        <Box  h={'full'}>
+          <Image w={"xl"} src={banner} />
+        </Box>
+      </HStack>
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
